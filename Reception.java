3b0abca8a94ccbb216838381df4adf6ddc98bd8b
SanduniKanishka <skchamodani@gmail.com>
2016-03-25 13:20:01 +0530

    Update Reception.java

--- Reception.java
+++ Reception.java
@@ -5,13 +5,14 @@ This is to recieve packet and send it to player class
 import java.net.* ;
 
 public class Reception extends Thread{
-    DatagramSocket socket;
+    MulticastSocket socket;
+    InetAddress host;
     int packetsize;
     InitiateComponents ic;
 	
-    public Reception(DatagramSocket socket,int packetsize,InitiateComponents ic){
+    public Reception(DatagramSocket socket,InetAddress host,int packetsize,InitiateComponents ic){
     	this.socket=socket;
-	
+	this.host=host;
 	this.ic=ic;
 	this.packetsize=packetsize;
 		
@@ -19,10 +20,14 @@ public class Reception extends Thread{
 		
     @Override
     public void run(){
+    	MulticastSocket socket = null;
 	Playback play=new Playback(ic);
         while(true){
             DatagramPacket packet = new DatagramPacket( new byte[packetsize], packetsize ) ;
             try{
+            	socket = new MulticastSocket(20000);
+		//InetAddress address = InetAddress.getByName("224.2.2.3");
+		socket.joinGroup(host);
            // Receive a packet (blocking)
                socket.receive( packet );
                play.captureAudio(packet.getData());//send thiS to the Playback class
